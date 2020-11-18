23dee7c10e6db1702e3ecf0d7e9ca437db3b03e8
Jonas <jdeblese@vub.be>
2020-11-18 12:12:07 +0100

    init

---
+++ src/main/scala/be/vub/Main.scala
@@ -0,0 +1,48 @@
+package be.vub
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    if(args.length == 3){
+      println("Start")
+      println("Get commits: ")
+        val commits = new Repository(args(0)).getCommits("master").take(args(1).toInt)
+      val edits = commits.flatMap(_.getAllConcreteEdits)
+      println("V"+commits.size)
+      println("Amount of edits : " + edits.length)
+      println("start clustering: ")
+      val cluster = new HierarchicalCluster(edits)
+      println("V")
+      println("create json: ")
+      val mode = args(2)
+      if(mode == "BigCluster"){
+        cluster.resultAsJSON()
+      } else if(mode == "RelevantCluster"){
+        cluster.clustersToJSON()
+      }
+      println("V")
+    } else if(args.length == 2){
+      println("Start")
+      println("Get commits: ")
+      val edits = new Repository(args(0)).getCommits("master").flatMap(_.getAllConcreteEdits)
+      println("V")
+      println("Amount of edits : " + edits.length)
+      println("start clustering: ")
+      val cluster = new HierarchicalCluster(edits)
+      println("V")
+      println("create json: ")
+      val mode = args(1)
+      if(mode == "BigCluster"){
+        cluster.resultAsJSON()
+      } else if(mode == "RelevantCluster"){
+        cluster.clustersToJSON()
+      }
+      println("V")
+    } else {
+      println("Unknown command")
+      println("available commands are : ")
+      println("- path (BigCluster|RelevantCluster)")
+      println("- path amount (BigCluster|RelevantCluster)")
+    }
+
+  }
+}
